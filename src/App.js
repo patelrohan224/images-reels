9b005af6991d155c00f25915ae478c35b019015c
Patel Rohan <tancentgame@gmail.com>
2024-12-15 16:07:44 +0530

    new branch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./view/Home.jsx";
-import Footer from "./view/Footer.jsx";
-import Looks from "./view/Looks.jsx";
-import Navbar from "./view/Navbar.jsx";
 
 const App = () => {
   return (
     <Router>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/looks/:id" element={<Looks />} />
+        <Route path="/" element={<div>/</div>} />
       </Routes>
-      <Footer />
     </Router>
   );
 };
